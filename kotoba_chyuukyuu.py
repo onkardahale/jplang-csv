97779ea05994c3940fb6eaf305427c3306c4d40f
Onkar Dahale <dahaleonkar@gmail.com>
2022-09-23 01:55:23 +0900

    intial commit
    Working mech to scrap jplang and write csv for use in Anki

---
+++ kotoba_chyuukyuu.py
@@ -0,0 +1,79 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+URL="https://jplang.tufs.ac.jp/int2/at/"
+
+headers_list = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"}
+
+#Returns soup for a url
+def req_soup(url):
+
+    try:
+        return BeautifulSoup(requests.get(url, headers=headers_list).text, "html.parser")
+    except requests.exceptions.ConnectionError:
+        return {"status":"404", "reason":"Connection error occurred"}
+    except requests.exceptions.ConnectTimeout:
+        return {"status":"408", "reason":"Connection Timeout"}
+    except:
+        return {"status":"418", "reason":"I'm a teapot"}
+
+
+#finds number of chapters
+def findChptMax():
+    try:
+        soup = req_soup(URL+"1/1.html").find("ul", {"class":"list-unstyled chapter-only"}).find_all("li")
+        return len(soup)
+    except:
+        print("失敗")
+
+#finds number of words in a chapter
+def findNWMax(NChpt):
+    try:
+        soup = req_soup(URL+str(NChpt)+"/"+str(NChpt)+".html").find("ul", {"class":"new_word_list list-group"}).find_all("li")
+        return len(soup)
+    except:
+        print("失敗")
+
+#writes csv for chapter
+def writeCsv(ChptName, NChpt):
+
+    soup = req_soup(URL+str(NChpt)+"/"+str(NChpt)+".html").find("ul", {"class", "new_word_list list-group"}).find_all("li")
+    soup = req_soup(URL+str(NChpt)+"/"+str(NChpt)+".html").find("ul", {"class":"new_word_list list-group"}).find_all("li", {"class":"list-group-item"})
+    NWMax = findNWMax(NChpt)
+
+    file = open(ChptName, 'w', encoding='UTF8', newline='')
+
+    writer = csv.writer(file)
+
+    for i in range(NWMax):
+
+        for li in soup:
+            row = []
+            front = ""
+            back = ""
+
+            wordSoup = li.find("span", {"class":"word"})
+            front = wordSoup.text.strip()
+            row.append(front)
+
+            yomikata = ""
+            imiTranslation = ""
+            span = li.find_all("button", {"class":"btn btn-xs btn-word"})
+            for s in span:
+                if s.text.strip() == "読み方":
+                    yomikata = s.findNext("span").text.strip()
+                if s.text.strip() == "意味":
+                    imiTranslation = s.findNext("span").text.strip().replace("英訳","")
+
+            back = yomikata+"\n"+imiTranslation
+
+            #append backside
+            row.append(back)
+
+            # write the data
+            writer.writerow(row)
+            print("Wrote "+ChptName+"...\n")
+
+    file.close()
+
---
+++ kotoba_shokyuu.py
@@ -0,0 +1,86 @@
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+URL="https://jplang.tufs.ac.jp/int2/at/"
+
+headers_list = {"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"}
+
+#Returns soup for a url
+def req_soup(url):
+
+    try:
+        return BeautifulSoup(requests.get(url, headers=headers_list).text, "html.parser")
+    except requests.exceptions.ConnectionError:
+        return {"status":"404", "reason":"Connection error occurred"}
+    except requests.exceptions.ConnectTimeout:
+        return {"status":"408", "reason":"Connection Timeout"}
+    except:
+        return {"status":"418", "reason":"I'm a teapot"}
+
+
+#finds number of chapters
+def findChptMax():
+    try:
+        soup = req_soup(URL+"1/1.html").find("ul", {"class":"list-unstyled chapter-only"}).find_all("li")
+        return len(soup)
+    except:
+        print("失敗")
+
+#finds number of words in a chapter
+def findNWMax(NChpt):
+    try:
+        soup = req_soup(URL+str(NChpt)+"/"+str(NChpt)+".html").find("ul", {"class":"new_word_list list-group"}).find_all("li")
+        return len(soup)
+    except:
+        print("失敗")
+
+#writes csv for chapter
+def writeCsv(ChptName, NChpt):
+
+    soup = req_soup(URL+str(NChpt)+"/"+str(NChpt)+".html").find("ul", {"class", "new_word_list list-group"}).find_all("li")
+    NWMax = findNWMax(NChpt)
+
+    file = open(ChptName, 'w', encoding='UTF8', newline='')
+
+    writer = csv.writer(file)
+
+    for i in range(NWMax):
+
+        for li in soup:
+            row = []
+            front = ""
+            back = ""
+            hasKanji = True
+
+            kanji = li.find("span", {"class": "word"})
+            if kanji is not None:
+                try:
+                    front = front + kanji.text.strip()
+                except:
+                    front =""
+                row.append(front)
+            else:
+                hasKanji = False
+
+            yomikata = li.find("span",{"class":"word-answer"})
+            if hasKanji == False:
+                front = front + yomikata.text.strip()
+                row.append(front)
+            else:
+                back = back + word.text.strip() + "\n"
+
+            imi = li.find("span",{"class":"textOverLine"})
+            try:
+                back = back + imi.text.strip()+"\n"
+            except:
+                back = back + ""
+
+            #append backside
+            row.append(back)
+
+            # write the data
+            writer.writerow(row)
+
+    file.close()
+
